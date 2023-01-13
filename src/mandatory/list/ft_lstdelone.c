f6b3477f9efa9e2ea3ca50eec8f3dadcb0515ddf
Kaio Matos <kaio_matos22@hotmail.com>
2023-01-13 20:23:12 -0300

    fix: linked list delone (following what was asked)

--- src/mandatory/list/ft_lstdelone.c
+++ src/mandatory/list/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 20:20:28 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/01/11 19:43:47 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/01/13 20:22:35 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*temp;
-
 	if (!lst || !del)
 		return ;
-	temp = lst;
-	lst = lst->next;
-	del(temp->content);
-	free(temp);
+	del(lst->content);
+	free(lst);
 }
