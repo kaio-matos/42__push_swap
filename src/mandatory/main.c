e71f4328ccef771b4e867ff17a9215309967d00f
Kaio Matos <kaio_matos22@hotmail.com>
2023-01-21 11:54:41 -0300

    feat: handle user input and treating it

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,73 +6,43 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 19:36:18 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/01/20 21:58:34 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:54:33 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-void	initiate_stack(t_stack **stack, int value);
+void	handle_program_validation(int argc, char **argv);
+void	handle_stk_fill(t_stack **a, char **stack);
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv)
 {
 	t_push_swap	*ps;
+	char		**stack;
 
-	ps = malloc(sizeof(t_push_swap));
+	handle_program_validation(argc, argv);
+	ps = salloc(sizeof(t_push_swap));
 	if (!ps)
-	{
-		ft_printf(ERROR);
-		return (1);
-	}
-	initiate_stack(&ps->a, 0);
-	ft_stkinit(&ps->b);
-
+		ft_exterr("malloc");
+	handle_stk_fill(&ps->a, ++argv);
 	ft_stkprint(ps->a);
-	ft_printf("------\n");
-
-	sa(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
-
-	pb(ps);
-	pb(ps);
-	pb(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
-
-	ra(ps);
-	rb(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
-
-	rra(ps);
-	rrb(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
-
-	sa(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
-
-	pa(ps);
-	pa(ps);
-	pa(ps);
-	ft_stkprint(ps->a);
-	ft_stkprint(ps->b);
-	ft_printf("------\n");
 }
 
-void	initiate_stack(t_stack **stack, int value)
+void	handle_program_validation(int argc, char **argv)
 {
-	*stack = ft_stknew(new_int(8 + value));
-	ft_stkpush(*stack, new_int(5 + value));
-	ft_stkpush(*stack, new_int(6 + value));
-	ft_stkpush(*stack, new_int(3 + value));
-	ft_stkpush(*stack, new_int(1 + value));
-	ft_stkpush(*stack, new_int(2 + value));
+	if (argc <= 1)
+		ft_exterr("Missing arguments\n");
+	while (*(++argv))
+		if (!ft_isdigit(**argv))
+			ft_exterr("Bad arguments, use only digits\n");
+}
+
+void	handle_stk_fill(t_stack **a, char **stack)
+{
+	ft_stkinit(a);
+	while (*stack)
+	{
+		ft_stkpush(*a, new_int(ft_atoi(*stack)));
+		stack++;
+	}
 }
