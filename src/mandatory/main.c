c6cac04232cb4f9a4d45c17459c993502c9b0c0d
Kaio Matos <kaio_matos22@hotmail.com>
2023-01-23 21:22:39 -0300

    fix: use ft_isinteget for argument checking

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 19:36:18 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/01/23 19:58:21 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/01/23 20:57:00 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ void	handle_program_validation(int argc, char **argv)
 	if (argc <= 1)
 		ft_exterr("Missing arguments\n");
 	while (*(++argv))
-		if (!ft_isdigit(**argv))
-			ft_exterr("Bad arguments, use only digits\n");
+		if (!ft_isinteger(*argv))
+			ft_exterr("Bad arguments, not an integer\n");
 }
 
 void	handle_stk_fill(t_stack **a, char **stack)
