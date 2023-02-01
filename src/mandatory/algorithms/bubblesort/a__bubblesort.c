ea7fcabf779b6840669bfaeda74066d953a751c9
Kaio Matos <kaio_matos22@hotmail.com>
2023-01-31 22:05:04 -0300

    refactor: swap void utility function

--- src/mandatory/algorithms/bubblesort/a__bubblesort.c
+++ src/mandatory/algorithms/bubblesort/a__bubblesort.c
@@ -6,11 +6,20 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:26:29 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/01/31 21:48:06 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/01/31 22:03:30 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
+
+void	swap_void(void **value, void **new)
+{
+	void	*swap;
+
+	swap = *value;
+	*value = *new;
+	*new = swap;
+}
 
 void	a__bubblesort(t_dlist *numbers)
 {
@@ -27,11 +36,7 @@ void	a__bubblesort(t_dlist *numbers)
 			while (changed)
 			{
 				if (get_int(saved->content) > get_int(changed->content))
-				{
-					swap = saved->content;
-					saved->content = changed->content;
-					changed->content = swap;
-				}
+					swap_void(&saved->content, &changed->content);
 				changed = changed->next;
 			}
 		}
