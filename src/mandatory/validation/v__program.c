926310478728f56e2c79cc7f8bcb8d8d02763d2a
Kaio Matos <kaio_matos22@hotmail.com>
2023-02-06 21:55:07 -0300

    validate repeated args numbers

--- src/mandatory/validation/v__program.c
+++ src/mandatory/validation/v__program.c
@@ -6,11 +6,13 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 20:24:41 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/02/06 21:42:36 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:54:19 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
+
+t_bool	has_repeated_numbers(char **numbers);
 
 char	**v__program(int argc, char **argv)
 {
@@ -29,5 +31,25 @@ char	**v__program(int argc, char **argv)
 			ft_free_matrix(numbers);
 			ft_exterr("Bad arguments, not an integer\n");
 		}
+	if (has_repeated_numbers(numbers))
+	{
+		ft_free_matrix(numbers);
+		ft_exterr("Bad arguments, the list cannot have repeated numbers\n");
+	}
+
 	return (numbers);
+}
+
+t_bool	has_repeated_numbers(char **numbers)
+{
+	char	**temp;
+
+	while (*(++numbers))
+	{
+		temp = numbers;
+		while (*(++temp))
+			if (ft_atoi(*numbers) == ft_atoi(*temp))
+				return (TRUE);
+	}
+	return (FALSE);
 }
