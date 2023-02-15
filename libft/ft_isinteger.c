5a1179551bb0519b323aedc3a51ef02c29760c0a
Kaio Matos <kaio_matos22@hotmail.com>
2023-02-14 21:16:05 -0300

    fix: is integer wasn't handling all cases

--- libft/ft_isinteger.c
+++ libft/ft_isinteger.c
@@ -6,34 +6,54 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 20:37:51 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/02/08 20:39:41 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/02/14 21:03:55 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int	ft_isinteger(char *string)
+int	is_less_or_plus(char c)
+{
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
+}
+
+int	is_integer(char *number, int sign)
 {
 	long long	converted;
-	int			sign;
-	int			i;
+
+	if ((ft_strlen(number) + sign) > MAX_INT_LENGTH)
+		return (0);
+	converted = ft_atol(number);
+	if (converted > __INT_MAX__ || converted < -__INT_MAX__ - 1)
+		return (0);
+	return (1);
+}
+
+int	ft_isinteger(char *string)
+{
+	int	sign;
+	int	i;
 
 	sign = 0;
 	i = 0;
 	if (*string == '-')
 		sign = -1;
-	while (string[i])
+	while (string[i] == '-' || string[i] == '+')
 	{
-		if (!ft_isdigit(string[i]) && string[i] != '-' && string[i] != '+')
-			return (0);
-		// if ((string[i] == '-' || string[i] == '+') && (string[i - 1] == '-' || string[i - 1] == '='))
-		// 	return (0);
+		if (is_less_or_plus(string[i]))
+			if (is_less_or_plus(string[i - 1]) || !string[i + 1])
+				return (0);
 		i++;
 	}
-	if ((ft_strlen(string) + sign) > MAX_INT_LENGTH)
-		return (0);
-	converted = ft_atol(string);
-	if (converted > __INT_MAX__ || converted < -__INT_MAX__ - 1)
+	while (string[i])
+	{
+		if (!ft_isdigit(string[i]))
+			return (0);
+		i++;
+	}
+	if (!is_integer(string, sign))
 		return (0);
 	return (1);
 }
