16fe2dd782669cfe326f34d0471fced33d41bdc7
Kaio Matos <kaio_matos22@hotmail.com>
2023-02-24 22:50:21 -0300

    chore: rename test to automatic_test and create generate number

---
+++ generate_numbers.js
@@ -0,0 +1,16 @@
+const generateNumbers = (stack_size = 10) => {
+	const getRandomNumber = () => Math.round(Math.random() * 10000)
+	let array = []
+
+	for (let i = 0; i <= stack_size; i++) {
+		let random = getRandomNumber()
+
+		do random = getRandomNumber()
+		while (array.filter(n => n === random).length !== 0)
+
+		array.push(random)
+	}
+	return array
+}
+
+console.log(generateNumbers(100).join(' '))
